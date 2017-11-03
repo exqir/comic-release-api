c1825bf760954270d5f6fb0d08e637f610324993
Malte Sellmer <malte.sellmer@me.com>
2017-11-03 13:00:51 +0100

    Revert "add comic-mongo-client"
    This reverts commit 7c5bc74a59691a6cb7da71ce8f089c3dcda5ef51.

--- index.js
+++ index.js
@@ -1,15 +1,23 @@
 'use strict'
 
-const dbClient = require('comic-mongo-client')
+const mongoose = require('mongoose')
+const Logger = require('./lib/Logger')
 const app = require('./lib/app')
 
 const dbServer = process.env.DB_SERVER || 'localhost'
-const dbName = process.env.DB || 'test'
+const db = process.env.DB || 'test'
 const port = process.env.PORT || 3000
 
+function connectToDatabase (url, user, pass) {
+  try {
+    mongoose.connect('mongodb://' + url, {'user': user, 'pass': pass})
+  } catch (e) {
+    Logger.logError('connection to database failed: ' + e)
+  }
+}
 // localhost/comic-app
-dbClient.connect(dbServer + '/' + dbName)
-app.db = dbClient.db
+connectToDatabase(dbServer + '/' + db)
+app.db = mongoose
 app.listen(port, function () {
   console.log('comic-release-api started \n listening on ' + port)
 })
