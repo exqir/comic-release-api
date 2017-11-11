8b325712b8b746466c36d81cd49dbe3b5ca89a04
Malte Sellmer <malte.sellmer@me.com>
2017-11-11 17:22:04 +0100

    basic working graphql api

--- index.js
+++ index.js
@@ -1,25 +1,15 @@
 'use strict'
 
-const mongoose = require('mongoose')
-const Logger = require('./lib/Logger')
-const app = require('./lib/app')
+const express = require('./lib/express')
+const mongo = require('./lib/mongo')
 
 const dbServer = process.env.DB_SERVER || 'localhost'
-const db = process.env.DB || 'test'
+const dbName = process.env.DB || 'test'
 const port = process.env.PORT || 3000
 
-function connectToDatabase (url, user, pass) {
-  try {
-    mongoose.connect('mongodb://' + url, {'user': user, 'pass': pass})
-  } catch (e) {
-    Logger.logError('connection to database failed: ' + e)
-  }
+const start = async () => {
+  const db = await mongo.connect(dbServer + '/' + dbName)
+  const server = express.start(port) 
 }
-// localhost/comic-app
-connectToDatabase(dbServer + '/' + db)
-app.db = mongoose
-app.listen(port, function () {
-  console.log('comic-release-api started \n listening on ' + port)
-})
 
-module.exports = app
+start ()
