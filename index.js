84252ae0d691ecd8f6606c183af8531af268df3c
Malte Sellmer <malte.sellmer@me.com>
2018-02-09 19:24:02 +0100

    enable testing with docker setup

--- index.js
+++ index.js
@@ -1,15 +1,8 @@
 'use strict'
 
-const express = require('./lib/express')
-const mongo = require('./lib/mongo')
+import { start } from './lib/app'
+import { express } from './lib/express'
 
-const dbServer = process.env.DB_SERVER || 'localhost'
-const dbName = process.env.DB || 'test'
-const port = process.env.PORT || 3000
+start()
 
-const start = async () => {
-  const db = await mongo.connect(dbServer + '/' + dbName)
-  const server = express.start(port) 
-}
-
-start ()
+export const app = express
